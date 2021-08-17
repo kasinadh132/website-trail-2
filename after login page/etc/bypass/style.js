6b69fa567ae238fc8d79fde71b42f83fbd38fbad
swayampakula.v.s.s.pavanakasinadhasarma <81065703+kasinadh132@users.noreply.github.com>
2021-08-17 17:36:35 +0530

    Add files via upload

---
+++ after login page/etc/bypass/style.js
@@ -0,0 +1,12 @@
+const signUpButton = document.getElementById('Sign up');
+const LoginButton = document.getElementById('Login');
+const container = document.getElementById('container');
+
+LoginButton.onclick = function() {
+    location.href = 'etc/bypass/index.html';
+}
+
+SignUp.onclick = function() {
+    location.href = 'etc/bypass/index.html';
+
+}
